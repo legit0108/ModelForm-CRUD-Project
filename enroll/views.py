7ca6e254f6c183e9d4846a5681303aae86a63872
legit0108 <legitgamer2345@gmail.com>
2023-01-14 16:12:55 +0530

    Added Bootstrap to Form, Post and Get between Form and Database

--- enroll/views.py
+++ enroll/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
+from .models import User
 
 # Create your views here.
 def addAndShow(request):
     if request.method == 'POST':
       form = StudentRegistration(request.POST)
+      if form.is_valid():
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        user = User(name=name, email=email)
+        user.save()
+        form = StudentRegistration() # to show blank form post submit
+        # form.save() can also use this, no need to instantiate User
     else:
       form = StudentRegistration()      
-      return render(request, 'enroll/addAndShow.html', {'form': form})
+      users = User.objects.all()
+    return render(request, 'enroll/addAndShow.html', {'form': form, 'users': users})
